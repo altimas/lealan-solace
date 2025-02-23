74357f13209b2d5c8cdeccec7d5d83b4422fb030
LeAlan Carter <lealancarter@gmail.com>
2025-02-23 17:09:15 -0600

    cleanup comments

--- src/components/AdvocatesTable.tsx
+++ src/components/AdvocatesTable.tsx
@@ -32,34 +32,3 @@ export default function AdvocatesSection({data}: Props) {
   );
 
 }
-
-{/* <table>
-        
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>City</th>
-        <th>Degree</th>
-        <th>Specialties</th>
-        <th>Years of Experience</th>
-        <th>Phone Number</th>
-      
-      <tbody>
-        {data.map((advocate) => {
-          return (
-            <tr>
-              <td>{advocate.firstName}</td>
-              <td>{advocate.lastName}</td>
-              <td>{advocate.city}</td>
-              <td>{advocate.degree}</td>
-              <td>
-                {advocate.specialties.map((s) => (
-                  <div>{s}</div>
-                ))}
-              </td>
-              <td>{advocate.yearsOfExperience}</td>
-              <td>{advocate.phoneNumber}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table> */}
